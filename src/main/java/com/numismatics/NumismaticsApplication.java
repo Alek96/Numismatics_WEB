0ae2fc349c163adb904694f7387df336364ae178
Alek96 <zamojski96@gmail.com>
2018-05-14 11:12:55 +0200

    Generate simple project

---
+++ src/main/java/com/numismatics/NumismaticsApplication.java
@@ -0,0 +1,12 @@
+package com.numismatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumismaticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NumismaticsApplication.class, args);
+    }
+}
