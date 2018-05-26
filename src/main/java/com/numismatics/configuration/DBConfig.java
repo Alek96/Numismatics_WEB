c1e80b6784c37a3cc94fe6e5b09605300f5b7ac7
Yahor Melnik <Egorpyp@gmail.com>
2018-05-26 11:54:20 +0100

    datasource + config

---
+++ src/main/java/com/numismatics/configuration/DBConfig.java
@@ -0,0 +1,13 @@
+package com.numismatics.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Eoller on 26-May-18.
+ */
+@Configuration
+public class DBConfig {
+
+
+}
