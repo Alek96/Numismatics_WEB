9b772566d22d3938d504f2f56c10a13499e16ee4
Peperzastey <przemyslaw.poljanski@gmail.com>
2018-06-09 17:45:44 +0200

    Add SpringMVC and mapping for /contact path.

---
+++ src/main/java/com/numismatics/controller/ContactController.java
@@ -0,0 +1,14 @@
+package com.numismatics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+
+    @RequestMapping("/contact")
+    String contact() {
+        return "contact";
+    }
+}
+
