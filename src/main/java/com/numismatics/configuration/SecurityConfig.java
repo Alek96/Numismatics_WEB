9abf6a87e5526aa6fa6443e98c73126f420c725f
Aleksander Zamojski <zamojski96@gmail.com>
2018-06-10 02:10:36 +0200

    Repair footer

--- src/main/java/com/numismatics/configuration/SecurityConfig.java
+++ src/main/java/com/numismatics/configuration/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
                     .permitAll();
 
     }
